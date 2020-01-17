91b124fb2249c8cf8a6d90cb876e1955b0be9021
nguyenmuoi157 <33494100+nguyenmuoi157@users.noreply.github.com>
2020-01-18 03:11:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def server_error(e):
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port='80')
